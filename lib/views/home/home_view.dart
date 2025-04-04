232c8ec0e6990b62f0c70961b6c123aa2ecdcffd
cafefarm-johnny <npcdja@gmail.com>
2025-04-05 04:00:33 +0900

    fix: ListTile 위젯을 Container 위젯으로 래핑

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -24,9 +24,11 @@ class HomeView extends ConsumerWidget {
             child: ListView.builder(
               itemCount: 30,
               itemBuilder: (_, index) {
-                return ListTile(
-                  title: Text('[서비스명]'),
-                  subtitle: Text('구독료 - 매 월 {day}일 결제'),
+                return Container(
+                  child: ListTile(
+                    title: Text('[서비스명]'),
+                    subtitle: Text('구독료 - 매 월 {day}일 결제'),
+                  ),
                 );
               },
             ),
