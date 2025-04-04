05dbdd22afb927d61cbe9b79f7050aeabc8eddc4
cafefarm-johnny <npcdja@gmail.com>
2025-04-05 02:28:30 +0900

    feat: 홈 뷰 초안 작성

---
+++ lib/views/home/home_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeView extends ConsumerWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('구독 관리 앱'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              '이번 달 총 구독 비용: 1,234',
+              style: const TextStyle(fontSize: 20),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (_, index) {
+                return ListTile(
+                  title: Text('[서비스명]'),
+                  subtitle: Text('구독료 - 매 월 {day}일 결제'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
